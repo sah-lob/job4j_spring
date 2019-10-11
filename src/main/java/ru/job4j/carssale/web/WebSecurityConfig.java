b50ef35de98da8cd06005dcce6084c5847b1b8a5
Alexander Lobachev <Sah-lob@ya.ru>
2019-10-11 12:19:00 +0300

    heroku_3

--- src/main/java/ru/job4j/carssale/web/WebSecurityConfig.java
+++ src/main/java/ru/job4j/carssale/web/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/loginValidate");
     }
 }
